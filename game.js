5be79289170edf7a356ef3c8e6fadc782f4888c9
priddeh <dubstep.prid@googlemail.com>
2016-06-05 11:26:41 +0100

    lemon fix

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var money = 2; //increment this by one every click
 var auto_money = 0; //automatically click once per second
-var cost = 2; //the cost of this should increase exponentially
+var lcost = 2;
+var cost = 4; //the cost of this should increase exponentially
 
 
 function update_total_money() {
@@ -9,16 +10,16 @@ function update_total_money() {
 }
 
 document.getElementById("lemon").onclick =    function() {  
-    if (money < cost) {
+    if (money < lcost) {
         return alert('need more money.');
     }
     auto_money++; 
-    money -= cost;
+    money -= lcost;
     cost = Math.pow(2, auto_money);  
     var e = document.getElementById("money_per_second");
     e.innerHTML = 'Money per second: ' + auto_money; 
     var e2 = document.getElementById("lemon");
-    e2.innerHTML = 'Lemonaid stand [PH] ' + cost;
+    e2.innerHTML = 'Lemonaid stand [PH] ' + lcost;
     update_total_money();
 };
 
