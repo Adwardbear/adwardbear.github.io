7340c35053519c8a72f54db6686cbc773c321204
priddeh <dubstep.prid@googlemail.com>
2016-06-05 11:22:17 +0100

    Revert "uhon"
    This reverts commit b335a06f03877df69c6d11bea82110876a4f1b3f.

--- game.js
+++ game.js
@@ -8,6 +8,20 @@ function update_total_money() {
     e.innerHTML = 'Money: ' + money;
 }
 
+document.getElementById("lemon").onclick =    function() {  
+    if (money < cost) {
+        return alert('need more money.');
+    }
+    auto_money++; 
+    money -= cost;
+    cost = Math.pow(2, auto_money);  
+    var e = document.getElementById("money_per_second");
+    e.innerHTML = 'Money per second: ' + auto_money; 
+    var e2 = document.getElementById("lemon");
+    e2.innerHTML = 'Lemonaid stand [PH] ' + cost;
+    update_total_money();
+};
+
 document.getElementById("newspaper").onclick =    function() {  
     if (money < cost) {
         return alert('need more money.');
