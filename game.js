d3a0642b3e38e9c3b9414e126a3cbcd4c73a7e30
priddeh <dubstep.prid@googlemail.com>
2016-06-05 13:05:55 +0100

    l

--- game.js
+++ game.js
@@ -8,11 +8,7 @@ function update_total_clicks() {
     e.innerHTML = 'Clicks: ' + clicks;
 }
 
-document.getElementById("click").onclick =    function() {  
-    clicks++; 
-    update_total_clicks(); //updates the text
-};
-document.getElementById("buy_click").onclick =    function() {  
+document.getElementById("profit1").onclick =    function() {  
     if (clicks < cost) {
         return alert('need more clicks.');
     }
@@ -21,7 +17,7 @@ document.getElementById("buy_click").onclick =    function() {
     cost = Math.pow(2, auto_clicks);  
     var e = document.getElementById("clicks_per_second");
     e.innerHTML = 'Clicks per second: ' + auto_clicks; 
-    var e2 = document.getElementById("buy_click");
+    var e2 = document.getElementById("profit1");
     e2.innerHTML = 'Buy an autoclick for ' + cost;
     update_total_clicks();
 };
