b9ad77de28880acd9481f6295a211ac485fd2ece
priddeh <dubstep.prid@googlemail.com>
2016-06-05 12:05:29 +0100

    fixes

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var money = 2; //increment this by one every click
 var auto_money = 0; //automatically click once per second
-var lcost = 2;
-var cost = 4; //the cost of this should increase exponentially
+var cost1 = 2;
+var cost2 = 4; //the cost of this should increase exponentially
 
 
 function update_total_money() {
@@ -10,30 +10,30 @@ function update_total_money() {
 }
 
 document.getElementById("profit1").onclick =    function() {  
-    if (money < lcost) {
+    if (money < cost1) {
         return alert('need more money.');
     }
     auto_money++; 
-    money -= lcost;
+    money -= cost1;
     lcost = Math.pow(2, auto_money);  
     var e = document.getElementById("money_per_second");
     e.innerHTML = 'Money per second: ' + auto_money; 
     var e2 = document.getElementById("profit1");
-    e2.innerHTML = 'profit1 [PH] Cost: ' + lcost;
+    e2.innerHTML = 'profit1 [PH] Cost: ' + cost1;
     update_total_money();
 }
 
 document.getElementById("profit2").onclick =    function() {  
-    if (money < cost) {
+    if (money < cost2) {
         return alert('need more money.');
     }
     auto_money++; 
-    money -= cost;
+    money -= cost2;
     cost = Math.pow(4, auto_money);  
     var e = document.getElementById("money_per_second");
     e.innerHTML = 'Money per second: ' + auto_money; 
     var e2 = document.getElementById("profit2");
-    e2.innerHTML = 'profit2 [PH] Cost: ' + cost;
+    e2.innerHTML = 'profit2 [PH] Cost: ' + cost2;
     update_total_money();
 }
 
