90ebb878fdc0e6dd599b6aaa82358de9f0aa0a64
priddeh <dubstep.prid@googlemail.com>
2016-06-05 11:10:52 +0100

    var updates

--- game.js
+++ game.js
@@ -1,32 +1,42 @@
-var clicks = 2; //increment this by one every click
-var auto_clicks = 0; //automatically click once per second
+var money = 2; //increment this by one every click
+var auto_money = 0; //automatically click once per second
 var cost = 2; //the cost of this should increase exponentially
 
 
-function update_total_clicks() {
-    var e = document.getElementById("total_clicks");
-    e.innerHTML = 'Clicks: ' + clicks;
+function update_total_money() {
+    var e = document.getElementById("total_money");
+    e.innerHTML = 'Money: ' + money;
 }
 
-document.getElementById("click").onclick =    function() {  
-    clicks++; 
-    update_total_clicks(); //updates the text
-};
-document.getElementById("buy_click").onclick =    function() {  
+document.getElementById("lemon").onclick =    function() {  
     if (clicks < cost) {
         return alert('need more money.');
     }
-    auto_clicks++; 
-    clicks -= cost;
-    cost = Math.pow(2, auto_clicks);  
-    var e = document.getElementById("clicks_per_second");
-    e.innerHTML = 'Money per second: ' + auto_clicks; 
-    var e2 = document.getElementById("buy_click");
+    auto_money++; 
+    money -= cost;
+    cost = Math.pow(2, auto_money);  
+    var e = document.getElementById("money_per_second");
+    e.innerHTML = 'Money per second: ' + auto_money; 
+    var e2 = document.getElementById("lemon");
+    e2.innerHTML = 'Lemonaid stand [PH] ' + cost;
+    update_total_money();
+};
+
+document.getElementById("newspaper").onclick =    function() {  
+    if (clicks < cost) {
+        return alert('need more money.');
+    }
+    auto_money++; 
+    money -= cost;
+    cost = Math.pow(2, auto_money);  
+    var e = document.getElementById("money_per_second");
+    e.innerHTML = 'Money per second: ' + auto_money; 
+    var e2 = document.getElementById("newspaper");
     e2.innerHTML = 'Newspaper stand [PH] ' + cost;
-    update_total_clicks();
+    update_total_money();
 };
 
 setInterval(function () { 
-    clicks += auto_clicks;
-    update_total_clicks(); 
-}, 100); //once per second use the auto clickers
+    money += auto_money;
+    update_total_money(); 
+}, 1000); //once per second use the auto clickers
