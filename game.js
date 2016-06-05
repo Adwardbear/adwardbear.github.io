e65c240654a4f16d71d6344910d7afaaada604db
priddeh <dubstep.prid@googlemail.com>
2016-06-05 13:27:11 +0100

    l

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var money = 1; //increment this by one every click
-var auto_clicks = 0; //automatically click once per second
-var cost = 1; //the cost of this should increase exponentially
+var auto1 = 0; //automatically click once per second
+var cost1 = 1; //the cost of this should increase exponentially
 
 
 function update_total_money() {
@@ -9,20 +9,20 @@ function update_total_money() {
 }
 
 document.getElementById("profit1").onclick =    function() {  
-    if (money < cost) {
+    if (money < cost1) {
         return alert('need more money.');
     }
-    auto_clicks++; 
-    money -= cost;
-    cost = Math.pow(2, auto_clicks);  
+    auto1++; 
+    money -= cost1;
+    cost1 = Math.pow(2, auto1);  
     var e = document.getElementById("MPS");
-    e.innerHTML = 'Money per second: ' + auto_clicks; 
+    e.innerHTML = 'Money per second: ' + auto1; 
     var e2 = document.getElementById("profit1");
-    e2.innerHTML = 'Profit1 - Cost: ' + cost;
+    e2.innerHTML = 'Profit1 - Cost: ' + cost1;
     update_total_money();
 };
 
 setInterval(function () { 
-    money += auto_clicks;
+    money += auto1;
     update_total_money(); 
 }, 10); //once per second use the auto clickers
