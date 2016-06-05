7f7f6cc752d3284c7caef7cf1c00edd0c6788ffe
priddeh <dubstep.prid@googlemail.com>
2016-06-05 13:30:19 +0100

    added profit2

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 var money = 1; //increment this by one every click
 var auto1 = 0; //automatically click once per second
 var cost1 = 1; //the cost of this should increase exponentially
+var auto2 = 0;
+var cost2 = 2;
 
 
 function update_total_money() {
@@ -19,6 +21,20 @@ document.getElementById("profit1").onclick =    function() {
     e.innerHTML = 'Money per second: ' + auto1; 
     var e2 = document.getElementById("profit1");
     e2.innerHTML = 'Profit1 - Cost: ' + cost1;
+    update_total_money();
+};
+
+document.getElementById("profit2").onclick =    function() {  
+    if (money < cost2) {
+        return alert('need more money.');
+    }
+    auto2++; 
+    money -= cost2;
+    cost2 = Math.pow(2, auto2);  
+    var e = document.getElementById("MPS");
+    e.innerHTML = 'Money per second: ' + auto1; 
+    var e2 = document.getElementById("profit2");
+    e2.innerHTML = 'Profit2 - Cost: ' + cost2;
     update_total_money();
 };
 
