f673260b4f8823db4b96b3d190502a5288383f5d
Adwardbear <adwardbear@gmail.com>
2016-06-06 13:38:56 +0100

    save

--- game.js
+++ game.js
@@ -13,29 +13,10 @@ function GameSave() {
 	}
 }
 
-function NewGame() {
-	this.name = "Our Game";
-	this.description = "Our Game's Description";
-	this.numbers = [];
-	for (var i=0;i<10;i++) {
-		this.numbers[i] = i;
-	}
-}
-
-var Game = new NewGame();
-
-window.onload = function() {
-	window.localStorage['SaveName'] = JSON.stringify(Game);
-	
-	window.GameTwo = JSON.parse(window.localStorage['SaveName']);
-	
-	document.getElementById("name").innerHTML = "Name: " + GameTwo.name;
-	document.getElementById("desc").innerHTML = "Description: " + GameTwo.description;
-	var numbers = "";
-	for (var i=0;i<10;i++) {
-		if (numbers.length == 0) {numbers = i;} else {numbers += ", " + i;}
-	}
-	document.getElementById("numbers").innerHTML = "Numbers: " + numbers;
+var save = {
+    game.money: game.money,
+    this.buildings: this.buildings
+    localStorage.setItem("save",JSON.stringify(save));
 }
 
 //The object declaration for buildings
@@ -90,4 +71,5 @@ function Build(id) {
 window.onload = function() {
 	InitBuildings();
 	window.game = new GameSave();
+    var savegame = JSON.parse(localStorage.getItem("save"));
 };
