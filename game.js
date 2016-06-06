085009095889e5c6a04da07091bda719385be42b
Adwardbear <adwardbear@gmail.com>
2016-06-06 12:39:53 +0100

    saving

--- game.js
+++ game.js
@@ -61,6 +61,31 @@ function Build(id) {
 	}
 }
 
+function NewGame() {
+	this.name = "Our Game";
+	this.description = "Our Game's Description";
+	this.numbers = [];
+	for (var i=0;i<10;i++) {
+		this.numbers[i] = i;
+	}
+}
+
+var Game = new NewGame();
+
+window.onload = function() {
+	window.localStorage['SaveName'] = JSON.stringify(Game);
+	
+	window.GameTwo = JSON.parse(window.localStorage['SaveName']);
+	
+	document.getElementById("name").innerHTML = "Name: " + GameTwo.name;
+	document.getElementById("desc").innerHTML = "Description: " + GameTwo.description;
+	var numbers = "";
+	for (var i=0;i<10;i++) {
+		if (numbers.length == 0) {numbers = i;} else {numbers += ", " + i;}
+	}
+	document.getElementById("numbers").innerHTML = "Numbers: " + numbers;
+}
+
 //Run this code once the page has loaded fully
 window.onload = function() {
 	InitBuildings();
