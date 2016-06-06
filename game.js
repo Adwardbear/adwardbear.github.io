4bd17bbe9e271214d4c71251e3cb64a1472678fc
Adwardbear <adwardbear@gmail.com>
2016-06-06 12:52:02 +0100

    save

--- game.js
+++ game.js
@@ -13,6 +13,33 @@ function GameSave() {
 	}
 }
 
+function NewGame() {
+	this.name = "Our Game";
+	this.description = "Our Game's Description";
+    this.money;
+    this.buildings;
+	this.numbers = [];
+	for (var i=0;i<10;i++) {
+		this.numbers[i] = i;
+	}
+}
+
+var Game = new NewGame();
+
+window.onload = function() {
+	window.localStorage['SaveName'] = JSON.stringify(Game);
+	
+	window.GameTwo = JSON.parse(window.localStorage['SaveName']);
+	
+	document.getElementById("name").innerHTML = "Name: " + GameTwo.name;
+	document.getElementById("desc").innerHTML = "Description: " + GameTwo.description;
+	var numbers = "";
+	for (var i=0;i<10;i++) {
+		if (numbers.length == 0) {numbers = i;} else {numbers += ", " + i;}
+	}
+	document.getElementById("numbers").innerHTML = "Numbers: " + numbers;
+}
+
 //The object declaration for buildings
 function Building() {
 	this.Name = "Lemonade Stand";
@@ -59,31 +86,6 @@ function Build(id) {
 		document.getElementById("money").innerHTML = game.money;
 		document.getElementById("Building1Qty").innerHTML = game.buildings[id];
 	}
-}
-
-function NewGame() {
-	this.name = "Our Game";
-	this.description = "Our Game's Description";
-	this.numbers = [];
-	for (var i=0;i<10;i++) {
-		this.numbers[i] = i;
-	}
-}
-
-var Game = new NewGame();
-
-window.onload = function() {
-	window.localStorage['SaveName'] = JSON.stringify(Game);
-	
-	window.GameTwo = JSON.parse(window.localStorage['SaveName']);
-	
-	document.getElementById("name").innerHTML = "Name: " + GameTwo.name;
-	document.getElementById("desc").innerHTML = "Description: " + GameTwo.description;
-	var numbers = "";
-	for (var i=0;i<10;i++) {
-		if (numbers.length == 0) {numbers = i;} else {numbers += ", " + i;}
-	}
-	document.getElementById("numbers").innerHTML = "Numbers: " + numbers;
 }
 
 //Run this code once the page has loaded fully
