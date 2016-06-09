415f0f8f7cd33959fc98acf8efeda8ed47cefd84
Adwardbear <adwardbear@gmail.com>
2016-06-09 13:12:33 +0100

    l

--- game.js
+++ game.js
@@ -1,23 +1,38 @@
 //Declaring variables
 var player = {
     money: 1,
-    totalMoney: 1
+    building1Qty: 0,
+    building1Cost: 1
 }
 
-function moneyClick() {
-    document.getElementById("clickClick").disabled = true; 
+function buyBuilding1() {
+    player.building1Cost = Math.floor(1 * Math.pow(1.1,player.building1Qty));
+    if(player.money >= player.building1Cost){
+        player.building1Qty = player.building1Qty +1;
+        player.money = player.money - player.building1Cost;
+        updateScreen();
+    }
+    player.building1Cost = Math.floor(1 * Math.pow(1.1,player.building1Qty));
+    updateScreen();
+}
+
+function runBuilding1() {
+    console.log('Command runBuilding1()');
     console.log('btn disabled');
+    document.getElementById("runBuilding1").disabled = true; 
+    console.log('Timeout 10 seconds');
+    setTimeout(building1Money,10000);
     console.log('bar running');
     var elem = document.getElementById("bar1");   
-    var width = 10;
-    var id = setInterval(frame, 25);
+    var width = 1;
+    console.log('Bar set at 100frames');
+    var id = setInterval(frame, 100);
     function frame() {
       if (width >= 100) {
         clearInterval(id);
       } else {
         width++; 
         elem.style.width = width + '%'; 
-        document.getElementById("label").innerHTML = width * 1  + '%';
           
     }
   }
@@ -25,9 +40,44 @@ console.log('command complete');
 
 }
 
-function addMoney() {
-    player.money++;
+function building1Money() {
     console.log('player.money++');
+    player.money++;
+    console.log('btn enabled');
+    document.getElementById("runBuilding1").disabled = false; 
+    updateScreen();
+}
+
+function runBuilding2() {
+    console.log('Command runBuilding2()');
+    console.log('btn disabled');
+    document.getElementById("runBuilding2").disabled = true; 
+    console.log('Timeout 10 seconds');
+    setTimeout(building2Money,10000);
+    console.log('bar running');
+    var elem = document.getElementById("bar2");   
+    var width = 1;
+    console.log('Bar set at 100frames');
+    var id = setInterval(frame, 100);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+          
+    }
+  }
+console.log('command complete');
+
+}
+
+
+function building2Money() {
+    console.log('player.money++');
+    player.money = player.money + 10;
+    console.log('btn enabled');
+    document.getElementById("runBuilding2").disabled = false; 
     updateScreen();
 }
 
@@ -60,6 +110,8 @@ if(localStorage.getItem("gameSave") === null){
 //Starting game logic
 function updateScreen() {
     document.getElementById("money").innerHTML = player.money;
+    document.getElementById("building1Qty").innerHTML = player.building1Qty;
+    document.getElementById("building1Cost").innerHTML = player.building1Cost;
     //$("#metal").html(player.metal);
     //$("#circuit").html(player.circuit);
     //$("#eps").html((player.generatorAmount * player.generatorConversions - (player.metalPerSecond * player.basicRobotConversions) * 2) - (player.circuitPerSecond * player.basicFactoryConversions) * 4);
